d450fb06a9a1e017da8c1fca295eea1f100e023d
benjamin.kuchcik <b.kuchcik@accenture.com>
2019-01-29 19:22:01 +0100

    fix configuration error on java display quote

--- tp-display-quote-v2-java/src/main/java/fr/isima/server/Application.java
+++ tp-display-quote-v2-java/src/main/java/fr/isima/server/Application.java
@@ -3,15 +3,14 @@ package fr.isima.server;
 
 import fr.isima.business.Quotes;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableAutoConfiguration
-@ComponentScan("fr.isima")
-@EnableWebMvc
-public class Application extends WebMvcAutoConfiguration {
+@Configuration
+public class Application  {
 
     @Bean
     public Quotes quotes() {
